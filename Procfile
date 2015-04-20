720c1f6977cfc8b9651ec6b2fbc0afe741633aff
antlockyer <antlockyer@gmail.com>
2015-04-20 18:17:11 +0100

    Trying to make a compojure app run in heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/chord-tone-standalone.jar clojure.main -m chord-tone.web
---
+++ test/chord_tone/web_test.clj
@@ -0,0 +1,6 @@
+(ns chord-tone.web-test
+  (:require [clojure.test :refer :all]
+            [chord-tone.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
